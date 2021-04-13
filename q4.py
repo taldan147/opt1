010625a82a5177fb86dcfaad1d1c7d6e72f6efab
taldan147 <tald96@gmailc.om>
2021-04-13 18:59:18 +0300

    done

--- q4.py
+++ q4.py
@@ -4,86 +4,55 @@ import numpy as np
 A = np.asarray([[2,1,2], [1,-2,1], [1,2,3], [1,1,1]])
 AT=A.transpose()
 ATA = (AT@A)
-
+print("ATA",ATA)
 b = np.asarray([[6],[1],[5],[2]])
-
-
 # ----------------------------------------------- a --------------------------------------------------
 L = LA.cholesky(ATA)
-LT = L.transpose()
-
-# A^T A=LL^T,x=(A^T A)^(-1) A^T b⇒ x=(LL^T )^(-1) A^Tb
-
-LLT = (L@LT)
-
-
-
+print("L", L)
+LLT = (L@L.transpose())
 ATb = (AT@b)
-
+print("ATb",ATb)
 LLTinv = LA.inv(LLT)
-
 x = LLTinv@ATb
-
-print(x)
-
+print("cholesky x" ,x)
 # ----------------------------------------------- b --------------------------------------------------
 
 # x=R^-1Q^Tb QR factorization
 
 q, r = np.linalg.qr(A)
+print("Q",q)
+print("R",r)
 
 x2 = LA.inv(r)@q.transpose()@b
-print(x2)
+print("QR x", x2)
 
 # SVD facorization x = Vy = Vsimga^-1U^Tb
 
-
-
 (U,S,V) = LA.svd(A, full_matrices=False)
-
 print("U",U)
 print("S", S)
 V=V.transpose()
 print("V",V)
 
-
 Utb = (U.transpose())@b
-print("utb" , Utb)
-
-
-
-
-
-# S= np.asarray([[S[0]],[S[1]],[S[2]]])
 
 y = np.asarray([Utb[0]/S[0], Utb[1]/S[1], Utb[2]/S[2]])
-# y = Utb1/S
 print("y",y)
-
 x3 = V@y
-print(x3)
-
+print("SVD x",x3)
 
 # ----------------------------------------------- 4c --------------------------------------------------
 
 r = (A@x) - b
-
 print("r", r)
-
 Atr = AT@r
-
 print("ATr", Atr)
 
 # ----------------------------------------------- 4d --------------------------------------------------
 
 W = np.asarray([[808,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]])
-
 ATWA = AT@W@A
-
 ATWAinv = LA.inv(ATWA)
-
 xw = ATWAinv@AT@W@b
-
 rw = (A@xw)-b
-
-print(rw)
+print("r w",rw)
--- q5.py
+++ q5.py
@@ -6,7 +6,7 @@ from numpy import linalg as LA
 
 def GramShmidthQR(A):
     n=len(A[0])
-    R = np.zeros((n,n), dtype=int)
+    R = np.zeros((n,n))
     R[0][0] = LA.norm(A[:,0])
     q1 = A[:,0]/R[0][0]
     Q = [q1]
@@ -18,11 +18,11 @@ def GramShmidthQR(A):
         R[i][i] = LA.norm(qi)
         qi = qi/R[i][i]
         Q.append(qi)
-    return R,np.asarray(Q, dtype=int).transpose()
+    return R,np.asarray(Q).transpose()
 
 def ModifiedGramShmidthQR(A):
     n=len(A[0])
-    R = np.zeros((n,n), dtype=int)
+    R = np.zeros((n,n))
     R[0][0] = LA.norm(A[:,0])
     q1 = A[:,0]/R[0][0]
     Q = [q1]
@@ -34,19 +34,45 @@ def ModifiedGramShmidthQR(A):
         R[i][i] = LA.norm(qi)
         qi = qi/R[i][i]
         Q.append(qi)
-    return R,Q
+    return R,np.asarray(Q).transpose()
 
-# epsilon = 1
-# A = np.asarray([[1,1,1], [epsilon, 0,0], [0,epsilon,0], [0,0,epsilon]])
-# R,Q = GramShmidthQR(A)
-# print("e=1")
-# print(R)
-# print(Q)
-
-epsilon = math.e**-10
+epsilon = 1
 A = np.asarray([[1,1,1], [epsilon, 0,0], [0,epsilon,0], [0,0,epsilon]])
-print(A)
-# R,Q = GramShmidthQR(A)
-# print("1e-10")
-# print(R)
-# print(Q)
+R1,Q1 = GramShmidthQR(A)
+print("R1", R1)
+print("Q1", Q1)
+
+epsilon = 10**-10
+A = np.asarray([[1,1,1], [epsilon, 0,0], [0,epsilon,0], [0,0,epsilon]])
+R2,Q2 = GramShmidthQR(A)
+print("R2",R2)
+print("Q2",Q2)
+
+epsilon = 1
+A = np.asarray([[1,1,1], [epsilon, 0,0], [0,epsilon,0], [0,0,epsilon]])
+R3,Q3 = ModifiedGramShmidthQR(A)
+print("R3",R3)
+print("Q3",Q3)
+
+epsilon = 10**-10
+A = np.asarray([[1,1,1], [epsilon, 0,0], [0,epsilon,0], [0,0,epsilon]])
+R4,Q4 = ModifiedGramShmidthQR(A)
+print("R4",R4)
+print("Q4",Q4)
+
+# ----------------------------------------------- c --------------------------------------------------
+
+norm1 = LA.norm((Q1.transpose()@Q1)-np.eye(3))
+print(norm1)
+
+norm2 = LA.norm((Q2.transpose()@Q2)-np.eye(3))
+print(norm2)
+
+norm3 = LA.norm((Q3.transpose()@Q3)-np.eye(3))
+print(norm3)
+
+norm4 = LA.norm((Q4.transpose()@Q4)-np.eye(3))
+print(norm4)
+
+
+
